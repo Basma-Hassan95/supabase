fd7998847503c8b833a35463ebca9e320df9c513
Basma-Hassan95 <basmakhan220@gmail.com>
2025-07-31 03:26:16 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+
+// import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://cuxzktmfddfiwbgvufid.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1eHprdG1mZGRmaXdiZ3Z1ZmlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5MDc3MzUsImV4cCI6MjA2OTQ4MzczNX0._4UKpGJa9xDMRkqO5NXXGii43yltxEh0z87GCRdtxQ8'
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
+
+let sbtn = document.getElementById("sbtn")
+sbtn.addEventListener('click',async() =>{
+    let email = document.getElementById('semail').value.trim()
+    let password = document.getElementById('spass').value.trim()
+    
+    const { data, error } = await supabase.auth.signUp({
+  email,
+  password,
+})
+
+if(error){
+    console.log('error =>',error);
+    
+}else{
+    console.log("data =>", data);
+    
+}
+})
